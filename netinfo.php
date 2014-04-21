4a7091900ff15e3ec33a27e07aca2e46727198dc
jaderampulla <jaderampulla@gmail.com>
2014-04-21 18:21:53 -0400

    New
    Adjusted GUI Options

--- netinfo.php
+++ netinfo.php
@@ -246,7 +246,7 @@
 					<td colspan="2"><input name="showarp" id="showarp" type="checkbox" <?php if($_POST['showarp']) echo "checked"; ?> />&nbsp;Show ARP table from router</td>
 				</tr>
 				<tr>
-					<td colspan="2"><input name="ignoredns" id="ignoredns" type="checkbox" onchange="ignoredns_changer()" <?php if($_POST['ignoredns']) echo "checked"; ?> />&nbsp;Ignore DNS (Can reduces script run time)</td>
+					<td colspan="2"><input name="ignoredns" id="ignoredns" type="checkbox" onchange="ignoredns_changer()" <?php if($_POST['ignoredns']) echo "checked"; ?> />&nbsp;Ignore DNS (Can reduce script run time)</td>
 				</tr>
 				<script type="text/javascript">
 				function ignoredns_changer() {
